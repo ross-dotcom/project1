2e318068f356f5c3bb516372d4931d9f611c7b38
ross-dotcom <wross843@gmail.com>
2019-12-30 17:47:01 +0200

    Updated script.

--- scrapetest.py
+++ scrapetest.py
@@ -4,6 +4,7 @@ from urllib.request import urlopen
 from urllib.error import HTTPError
 from urllib.error import URLError
 from bs4 import BeautifulSoup
+import requests
 
 try:
     html = urlopen('https://www.soccerstats.com/latest.asp?league=england')
@@ -13,15 +14,30 @@ except URLError as e:
     print('The server could not be found.')
 else:
     print('It worked!')
-    
-bs = BeautifulSoup(html.read(), 'html.parser')
 
-epl_dict = dict()
+# url = 'https://www.soccerstats.com/latest.asp?league=england'
+# response = requests.get(url)
+# 
+# if response.status_code in [200]:
+#     print('It worked!')
+#     
+# html = response.text
+    
+bs = BeautifulSoup(html, 'html.parser')
+
+teams  = []
+pts = []
 
 teamList = bs.findAll('td', {'width':'110'})
 pointList = bs.findAll('td', {'bgcolor':'#dedede'})
 for team in teamList:
     t = team.get_text().strip()
-    epl_dict[t] = ''
+    teams.append(t)
+    
+for points in pointList:
+    p = points.get_text().strip()
+    pts.append(p)
+
+epl_dict = dict(zip(teams, pts))
 
 print(epl_dict)
