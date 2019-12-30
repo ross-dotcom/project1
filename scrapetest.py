6838a424f5201678438b425b0ab34346214278c7
ross-dotcom <wross843@gmail.com>
2019-12-30 23:21:56 +0200

    Code update.

--- scrapetest.py
+++ scrapetest.py
@@ -4,24 +4,25 @@ from urllib.request import urlopen
 from urllib.error import HTTPError
 from urllib.error import URLError
 from bs4 import BeautifulSoup
+from lxml import etree
 import requests
 
-try:
-    html = urlopen('https://www.soccerstats.com/latest.asp?league=england')
-except HTTPError as e:
-    print(e)
-except URLError as e:
-    print('The server could not be found.')
-else:
-    print('It worked!')
-
-# url = 'https://www.soccerstats.com/latest.asp?league=england'
-# response = requests.get(url)
-# 
-# if response.status_code in [200]:
+# try:
+#     html = urlopen('https://www.soccerstats.com/latest.asp?league=england')
+# except HTTPError as e:
+#     print(e)
+# except URLError as e:
+#     print('The server could not be found.')
+# else:
 #     print('It worked!')
-#     
-# html = response.text
+
+url = 'https://www.soccerstats.com/latest.asp?league=england'
+response = requests.get(url)
+
+if response.status_code in [200]:
+    print('It worked!')
+    
+html = response.text
     
 bs = BeautifulSoup(html, 'html.parser')
 
@@ -31,8 +32,8 @@ pts = []
 teamList = bs.findAll('td', {'width':'110'})
 pointList = bs.findAll('td', {'bgcolor':'#dedede'})
 for team in teamList:
-    t = team.get_text().strip()
-    teams.append(t)
+   t = team.get_text().strip()
+   teams.append(t)
     
 for points in pointList:
     p = points.get_text().strip()
