9b7275aa763bff23bd84fbb3d537a93c2d17334b
ross-dotcom <wross843@gmail.com>
2019-12-31 15:08:45 +0200

    Update.

--- scrapetest.py
+++ scrapetest.py
@@ -1,36 +1,24 @@
 #!/usr/bin/env python3
 
-from urllib.request import urlopen
-from urllib.error import HTTPError
-from urllib.error import URLError
 from bs4 import BeautifulSoup
-from lxml import etree
 import requests
 
-# try:
-#     html = urlopen('https://www.soccerstats.com/latest.asp?league=england')
-# except HTTPError as e:
-#     print(e)
-# except URLError as e:
-#     print('The server could not be found.')
-# else:
-#     print('It worked!')
-
-url = 'https://www.soccerstats.com/latest.asp?league=england'
-response = requests.get(url)
+url = 'https://www.soccerstats.com/latest.asp?league=england' #URL
+response = requests.get(url) #RESPONSE
 
 if response.status_code in [200]:
     print('It worked!')
+else:
+   print('Failed.')
     
-html = response.text
-    
-bs = BeautifulSoup(html, 'html.parser')
+bs = BeautifulSoup(response.text, 'html.parser') #CONTENT
 
 teams  = []
 pts = []
 
 teamList = bs.findAll('td', {'width':'110'})
 pointList = bs.findAll('td', {'bgcolor':'#dedede'})
+
 for team in teamList:
    t = team.get_text().strip()
    teams.append(t)
